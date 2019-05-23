ac0cdbed1711395029dec98a882ab82e3ea00ae8
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-23 12:23:21 -0500

    added auth back don't merge

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const morgan = require('morgan')
 const authRouter = require("../routers/authRouter.js");
 const usersRouter = require("../routers/usersRouter.js");
-// const authorization = require("../routers/authorization.js");
+const authorization = require("../routers/authorization.js");
 const tools =require('../routers/toolsRouter')
 const toolRental = require('../routers/rentedToolRouter')
 
@@ -18,9 +18,9 @@ server.use(express.json());
 
 
 server.use("/api/auth",  authRouter);
-server.use("/api/users",   usersRouter);
-server.use('/api/tools',  tools)
-server.use('/api/toolrental',  toolRental)
+server.use("/api/users",  authorization, usersRouter);
+server.use('/api/tools', authorization, tools)
+server.use('/api/toolrental', authorization, toolRental)
 
 
 
