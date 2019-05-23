cc28090c727b4026cf77dc890815575b98cfac8c
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-23 11:00:49 -0500

    manually added seed data

--- data/seeds/002tools.js
+++ data/seeds/002tools.js
@@ -1,28 +1,78 @@
-const faker =require('faker')
 
-function createTool() {
-  return {
-    user_id:"1",    
-    tool:faker.lorem.word(),
-    description:faker.lorem.words(),
-    image_url:"http://lorempixel.com/640/480",
-    is_rented:false
-  };
-}
-
-
-toolList = [];
-
-for (let i = 1; i < 40; i++) {
-  toolList.push(createTool());
-}
-
+    
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  exports.seed = function(knex, Promise) {
-    return knex("tools").then(function() {
+  return knex("tools")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex("tools").insert(toolList);
+      return knex("tools").insert([
+        {
+          tool: "Hammer",
+         image_url:
+            "http://lorempixel.com/640/480",
+          price: 3.00,
+          description:"I hit things",
+          user_id: 1
+        },
+        {
+          tool: "Screwdriver",
+         image_url:
+            "http://lorempixel.com/640/480",
+          price: 12.34,
+          description:"I screw things",
+         user_id: 2
+        },
+        {
+          tool: "drill",
+         image_url:
+            "http://lorempixel.com/640/480",
+          price: 2.3,
+          description:"I drill things",
+         user_id: 3
+        },
+        {
+          tool: "Angle Drill",
+         image_url:
+            "http://lorempixel.com/640/480",
+            description:"I drill things at an angle",
+          price: 10.00,
+         user_id: 4
+        },
+        {
+          tool: "Sledgehammer",
+         image_url:
+            "hhttp://lorempixel.com/640/480",
+          price: 5.42,
+          description:"I hit things very hard",
+         user_id: 5
+        },
+        {
+          tool: "Allen wrench",
+         image_url:
+            "http://lorempixel.com/640/480",
+          price: 9.34,
+          description:"I tighten things",
+         user_id: 6
+        },
+        {
+          tool: "Jackhammer",
+         image_url:
+            "http://lorempixel.com/640/480",
+          price: 8.00,
+          description:"I bust out concrete ",
+         user_id: 7
+        },
+        {
+          tool: "Impact drill",
+         image_url:
+            "http://lorempixel.com/640/480",
+            description:"I drill AND hit things",
+          price: 6.21,
+         user_id: 8
+        }
+        
+      ]);
     });
-  };
-}
+};
+
